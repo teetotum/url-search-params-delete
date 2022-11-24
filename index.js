3e3929da196305936ab22bef676edac1878911c2
Martin Winkler <martin.winkler@avenga.com>
2022-11-24 09:07:56 +0100

    added bugfix: now parameter order is stable when delete is called

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
     const originalDeleteFunction = URLSearchParams.prototype.delete;
 
     URLSearchParams.prototype.delete = function (key, value) {
-      const iterator = URLSearchParams.prototype.entries.call(this);
-      const entries = [...iterator];
-      const toBeRestored = entries.filter(([k, v]) => k === key && v !== value);
-      originalDeleteFunction.call(this, key);
+      const entriesIterator = URLSearchParams.prototype.entries.call(this);
+      const entries = [...entriesIterator];
+      const toBeRestored = entries.filter(
+        ([k, v]) => !(k === key && v === value)
+      );
+      const keysIterator = URLSearchParams.prototype.keys.call(this);
+      const keys = [...keysIterator];
+      keys.forEach((k) => originalDeleteFunction.call(this, k));
       toBeRestored.forEach(([k, v]) =>
         URLSearchParams.prototype.append.call(this, k, v)
       );
